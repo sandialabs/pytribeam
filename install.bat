01fae2795ec84e12c9f1e5142d328313e5c3f274
atpolonsky <apolon@sandia.gov>
2025-04-10 16:31:55 -0700

    Update install.bat
    updated installation procedure

--- install.bat
+++ install.bat
@@ -1,35 +1,10 @@
 echo off
-:begin
-echo Select an install mode for pyTriBeam:
-echo 1: Client install
-echo 2: Developer install
-echo:
-set /p op=Type option:
-if "%op%"=="1" goto client
-if "%op%"=="2" goto developer
 
-echo:
-echo Error, invalid response
-echo:
-cmd /k
-
-:client
-echo Installing for client use...
-"C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\pip-24.0-py3-none-any.whl
-"C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\setuptools-69.5.1-py3-none-any.whl
-for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
-for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
-"C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install . --no-index --no-build-isolation
-goto end
-
-:developer
-echo Installing for developer use...
+echo Installing pytribeam...
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\pip-24.0-py3-none-any.whl
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\setuptools-69.5.1-py3-none-any.whl
 for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
 for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install -e . --no-index --no-build-isolation
-goto end 
 
-:end
 cmd /k
