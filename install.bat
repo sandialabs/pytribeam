080bd74363c1890942e621097401e63c44ebc997
James Lamb <jdlamb@sandia.gov>
2026-04-08 15:04:43 -0600

    Updating wheels for hatchling

--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@ echo off
 echo Installing pytribeam...
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\pip-24.0-py3-none-any.whl
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %~dp0\wheelhouse\setuptools-69.5.1-py3-none-any.whl
-for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
-for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x
+for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x --no-index
+for %%x in (%~dp0\wheelhouse\*.whl) do "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install %%x --no-index
 "C:/Program Files/Enthought/Python/envs/Autoscript/python.exe" -m pip install -e . --no-index --no-build-isolation
 
 cmd /k
