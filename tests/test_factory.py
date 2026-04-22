1d537ca44b9534fa93635117d713466e3c69c00c
James Lamb <jdlamb@sandia.gov>
2026-04-22 17:38:04 -0600

    Fixing casting Path to string

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -24,7 +24,7 @@ import pytribeam.utilities as ut
 def test_dir() -> str:
     """The relative path and file string locating the default yml test file."""
 
-    return str(Path(__file__).parent.joinpath("files"))
+    return Path(__file__).parent.joinpath("files")
 
 
 @ut.run_on_microscope_machine
