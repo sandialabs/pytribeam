16d61240f620c351438131db05a2fcaff733dc2c
Chad Hovey <hovey@me.com>
2026-07-01 16:24:42 -0600

    chore: update _version.py pre-release

--- src/pytribeam/_version.py
+++ src/pytribeam/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.0.9rc3.dev15'
-__version_tuple__ = version_tuple = (0, 0, 9, 'rc3', 'dev15')
+__version__ = version = '0.0.9rc4.dev7'
+__version_tuple__ = version_tuple = (0, 0, 9, 'rc4', 'dev7')
 
 __commit_id__ = commit_id = None
